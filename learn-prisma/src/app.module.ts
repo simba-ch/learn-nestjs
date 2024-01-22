4aaecba91770e7fbff19daa379a8f947f1c22443
simba <18721784110@163.com>
2024-01-22 18:19:36 +0800

    添加category模块

--- learn-prisma/src/app.module.ts
+++ learn-prisma/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
+import { CategoryModule } from './category/category.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, CategoryModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- learn-prisma/src/app.service.ts
+++ learn-prisma/src/app.service.ts
@@ -1,8 +1,57 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from './prisma/prisma.service';
 
 @Injectable()
 export class AppService {
+
+  constructor(private readonly prismaService: PrismaService) { }
   getHello(): string {
     return 'Hello World!';
+  }
+
+  getPost() {
+    return this.prismaService.post.findFirst({
+      where: {
+        id: "falfkl;akflsd"
+      },
+      include: {
+        categories: {
+          include: {
+            category: true
+          }
+        }
+      }
+
+    })
+  }
+
+  createPost() {
+    this.prismaService.post.create({
+      data: {
+        title: "How to be Bob",
+        author: {
+          connect: {
+            id: ""
+          }
+        },
+        categories: {
+          create: [
+            {
+              assignedAt: new Date(),
+              assignedBy: "Bob",
+
+              category: {
+
+                create: {
+
+                  name: "New category",
+
+                }
+              }
+            }
+          ]
+        }
+      }
+    })
   }
 }
