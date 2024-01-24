9af66d8d96f19377673f1edc418b529a636a2955
simba <18721784110@163.com>
2024-01-24 18:16:59 +0800

    进程：高级类型安全

--- learn-prisma/src/app.service.ts
+++ learn-prisma/src/app.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from './prisma/prisma.service';
-
+import { Prisma } from "@prisma/client";
 @Injectable()
 export class AppService {
 
@@ -54,4 +54,7 @@ export class AppService {
       }
     })
   }
+
+
+
 }
