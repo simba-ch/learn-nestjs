ccf1aae974549aaf1bfd967fb905cbf0bdd10f9a
simba <18721784110@163.com>
2024-01-25 18:02:20 +0800

    聚合

--- learn-prisma/src/app.service.ts
+++ learn-prisma/src/app.service.ts
@@ -4,57 +4,70 @@ import { Prisma } from "@prisma/client";
 @Injectable()
 export class AppService {
 
-  constructor(private readonly prismaService: PrismaService) { }
+  constructor(private readonly prismaService: PrismaService) {
+    this.prismaService.$extends({
+      name:"signUp",
+      model:{
+        user:{},
+        post:{}
+      }
+    })
+   }
   getHello(): string {
     return 'Hello World!';
   }
 
-  getPost() {
-    return this.prismaService.post.findFirst({
-      where: {
-        id: "falfkl;akflsd"
-      },
-      include: {
-        categories: {
-          include: {
-            category: true
-          }
-        }
-      }
+  // getPost() {
+  //   return this.prismaService.post.findFirst({
+  //     where: {
+  //       id: "falfkl;akflsd"
+  //     },
+  //     include: {
+  //       categories: {
+  //         include: {
+  //           category: true
+  //         }
+  //       }
+  //     }
 
+  //   })
+  // }
+
+  // createPost() {
+  //   this.prismaService.post.create({
+  //     data: {
+  //       title: "How to be Bob",
+  //       author: {
+  //         connect: {
+  //           id: ""
+  //         }
+  //       },
+  //       categories: {
+  //         create: [
+  //           {
+  //             assignedAt: new Date(),
+  //             assignedBy: "Bob",
+
+  //             category: {
+
+  //               create: {
+
+  //                 name: "New category",
+
+  //               }
+  //             }
+  //           }
+  //         ]
+  //       }
+  //     }
+  //   })
+  // }
+
+
+
+  temp() {
+    Prisma.validator<Prisma.UserDefaultArgs>()({
+      select: { email: true, name: true }
     })
   }
-
-  createPost() {
-    this.prismaService.post.create({
-      data: {
-        title: "How to be Bob",
-        author: {
-          connect: {
-            id: ""
-          }
-        },
-        categories: {
-          create: [
-            {
-              assignedAt: new Date(),
-              assignedBy: "Bob",
-
-              category: {
-
-                create: {
-
-                  name: "New category",
-
-                }
-              }
-            }
-          ]
-        }
-      }
-    })
-  }
-
-
-
 }
