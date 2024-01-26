30b099a1b648851fbaf0ec1ced468728254bc46e
simba <18721784110@163.com>
2024-01-26 18:15:25 +0800

    进程：追踪

--- learn-prisma/src/prisma/prisma.service.ts
+++ learn-prisma/src/prisma/prisma.service.ts
@@ -2,6 +2,17 @@ import { Injectable, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
+    constructor(){
+        super({
+            log:[
+                {
+                    emit:"event",
+                    level:"query"
+                }
+            ]
+        })
+       this.$extends({})
+    }
     async onModuleInit() {
         await this.$connect()
     }
